b2277f84cf6cf95916b076b68e8708b6a717e91e
Kevin Caliboso <kevin.caliboso@adish.com.ph>
2023-11-10 16:25:50 +0800

    created an auth layout for the nav

---
+++ layouts/auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <TheNav />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import TheNav from '~/components/Shared/TheNav.vue'
+export default {
+  components: {
+    TheNav,
+  },
+}
+</script>
