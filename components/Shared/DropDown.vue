8e86773462116ff6c3b91f93437e729b257742f0
Kevin Caliboso <kevin.caliboso@adish.com.ph>
2023-11-13 15:34:42 +0800

    created dropdown component for forms

---
+++ components/Shared/DropDown.vue
@@ -0,0 +1,23 @@
+<template>
+  <select
+    id="userType"
+    name="userType"
+    :value="value"
+    @change="$emit('input', $event.target.value)"
+  >
+    <option value="user" selected>User</option>
+    <option value="moderator">Moderator</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'DropDown',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
