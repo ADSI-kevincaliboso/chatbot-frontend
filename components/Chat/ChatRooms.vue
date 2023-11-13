18199ae365c76972fd5e4fc8370da42519142638
Kevin Caliboso <kevin.caliboso@adish.com.ph>
2023-11-13 15:34:21 +0800

    created chatroom table component to show chatroom lists from backend

---
+++ components/Chat/ChatRooms.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Chatrooms</h1>
+
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Chatroom Name</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="chatroom in chatrooms" :key="chatroom.id">
+            <td>{{ chatroom.id }}</td>
+            <td>{{ chatroom.name }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatRooms',
+  props: {
+    chatrooms: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
