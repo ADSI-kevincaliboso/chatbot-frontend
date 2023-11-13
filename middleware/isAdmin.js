9d15dcef37c0392e57b2a91b743400ca0cc2fd15
Kevin Caliboso <kevin.caliboso@adish.com.ph>
2023-11-13 15:35:21 +0800

    added redirection on isAdmin middleware

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,7 +1,15 @@
+/* eslint-disable no-lonely-if */
 export default function (context) {
   if (!process.client) {
     if (
-      context.store.getters.userType !== 'admin' ||
+      context.store.getters.userType !== 'admin' &&
+      context.store.getters.userType !== 'moderator'
+    ) {
+      context.redirect('/chatbot')
+    }
+  } else {
+    if (
+      context.store.getters.userType !== 'admin' &&
       context.store.getters.userType !== 'moderator'
     ) {
       context.redirect('/chatbot')
