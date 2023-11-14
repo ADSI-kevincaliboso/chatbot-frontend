dc1fbfde635d8e74f1dda77e4216f827ef7acd9b
Kevin Caliboso <kevin.caliboso@adish.com.ph>
2023-11-14 13:07:14 +0800

    removed asyncData on admin vue and added event emit from sidenav

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Dashboard</h1>
     <br />
-    <TheSideNav />
+    <TheSideNav :rooms="rooms" @chatroomRefresh="refresh" />
   </div>
 </template>
 
@@ -18,25 +18,37 @@ export default {
   },
   layout: 'auth',
   middleware: ['checkAuth', 'auth', 'isAdmin'],
-  // asyncData(context) {
-  //   return axios
-  //     .get(`${process.env.baseUrl}/chat-rooms/`, {
-  //       headers: {
-  //         Accept: 'application/json',
-  //         Authorization: `Bearer ${context.store.getters.token}`,
-  //       },
-  //     })
-  //     .then((res) => {
-  //       return {
-  //         chatrooms: res.data,
-  //       }
-  //     })
-  //     .catch((e) => new Error('Chatroom error'))
-  // },
-  // computed: {
-  //   chatroomList() {
-  //     return this.chatrooms
-  //   },
-  // },
+  data() {
+    return {
+      chatrooms: [],
+    }
+  },
+  computed: {
+    rooms() {
+      return this.chatrooms
+    },
+  },
+  mounted() {
+    this.connect()
+  },
+  methods: {
+    connect() {
+      this.$echo.connector.options.auth.headers.Authorization = `Bearer ${this.$store.getters.token}`
+
+      const privateChannel = this.$echo.private(`chatrooms`)
+      privateChannel.listen('.chatroom.new', (event) => {
+        this.chatrooms.push(event.chatroom)
+      })
+
+      privateChannel.listen('.chatroom.destroy', (event) => {
+        this.chatrooms = this.chatrooms.filter(
+          (chatroom) => chatroom.id !== event.chatroom.id
+        )
+      })
+    },
+    refresh($event) {
+      this.chatrooms = [...$event]
+    },
+  },
 }
 </script>
