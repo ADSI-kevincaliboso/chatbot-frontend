f7cfd67d5da3dedffc987645b646463c985af6ac
Kevin Caliboso <kevin.caliboso@adish.com.ph>
2023-11-14 13:04:43 +0800

    added router redirect when message is not sent due to error on axios

--- components/Chat/ChatMessage.vue
+++ components/Chat/ChatMessage.vue
@@ -22,19 +22,29 @@ export default {
     async sendMessage() {
       // send message then emit a event that the message is sent
       if (this.message) {
-        const res = await axios.post(
-          `${process.env.baseUrl}/chat/room/${this.$store.getters.chatroom}/message`,
-          {
-            message: this.message,
-          },
-          {
-            headers: {
-              Accept: 'application/json',
-              Authorization: `Bearer ${this.$store.getters.token}`,
-              'X-Socket-ID': this.$echo.socketId(),
+        const res = await axios
+          .post(
+            `${process.env.baseUrl}/chat/room/${this.$store.getters.chatroom}/message`,
+            {
+              message: this.message,
             },
-          }
-        )
+            {
+              headers: {
+                Accept: 'application/json',
+                Authorization: `Bearer ${this.$store.getters.token}`,
+                'X-Socket-ID': this.$echo.socketId(),
+              },
+            }
+          )
+          .catch((_error) => {
+            const userType = this.$store.getters.userType
+
+            if (userType === 'admin') {
+              this.$router.push('/admin')
+            } else {
+              this.$router.push('/chatbot')
+            }
+          })
 
         this.message = ''
 
