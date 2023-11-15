a45f1076ccaf46e367b5330e64e8e45008d769ef
Kevin Caliboso <kevin.caliboso@adish.com.ph>
2023-11-15 16:47:01 +0800

    changed title of nuxt app

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'chatbot-frontend',
+    title: 'Chatbot',
     htmlAttrs: {
       lang: 'en',
     },
